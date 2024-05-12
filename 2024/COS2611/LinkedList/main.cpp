0bcf232199326bb99afe7ec0e84b82d9dbe375ca
Nhlanhla Mkhize <mbmkhize22@gmail.com>
2024-05-12 13:44:58 +0200

    How to insert a new node in a linked list in C++? (at the front, at the end, after a given node)

---
+++ 2024/COS2611/LinkedList/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public:
+    int value;
+    Node* Next;
+};
+
+void printList(Node*n) {
+    while(n!=NULL) {
+        cout << n->value << endl;
+        n = n->Next;
+    }
+}
+
+// Inserting pointer to a pointer(**)
+void insertAtTheFront(Node**head, int newValue) {
+    //Steps
+    //1. prepare new node
+    Node* newNode = new Node();
+    newNode->value = newValue;
+
+    //2. Put it in front of the current head
+    newNode->Next = *head;
+
+    //3. Move head of the list to point to the new node
+    *head = newNode;
+}
+
+void insertAtTheEnd(Node**head, int newValue) {
+    // 1. prepare a new node
+    Node* newNode = new Node();
+    newNode->value = newValue;
+    newNode->Next = NULL;
+
+    //2. If linked list is empty, newNode will be a head node
+    if(*head == NULL) {
+        *head = newNode;
+        return;
+    }
+
+    //3. Find the last node
+    Node* last = *head;
+    while(last->Next != NULL) {
+        last = last->Next;
+    }
+
+    //4. Insert newNode after last node(at the end)
+    last->Next = newNode;
+}
+
+int main()
+{
+    Node* head = new Node();
+    Node* second = new Node();
+    Node* third = new Node();
+
+    head->value = 1;
+    head->Next = second;
+
+    second->value = 2;
+    second->Next = third;
+
+    third->value = 3;
+    third->Next = NULL;
+
+    insertAtTheFront(&head, -1);
+    insertAtTheEnd(&head, 4);
+    insertAtTheEnd(&head, 5);
+    printList(head);
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
